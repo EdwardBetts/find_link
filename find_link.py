08087daff0220d8e5f59cc3faacb0bf45d689ec7
Edward Betts <edwardbetts@gmail.com>
2012-10-07 14:35:35 +0100

    add javascript diff view

--- find_link.py
+++ find_link.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, Markup, redirect, url_for
 from time import time
 from datetime import datetime
+from pprint import pprint
 import urllib, json, re, os, sys
 
 app = Flask(__name__)
@@ -10,13 +11,14 @@ key = open(__file__[:last_slash+1] + 'key').read()
 if key[-1] == '\n':
     key = key[:-1]
 Flask.secret_key = key
-query_url = 'http://en.wikipedia.org/w/api.php?format=json&action=query&'
+query_url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&'
 #srprop = 'size|wordcount|timestamp|score|snippet|titlesnippet|sectionsnippet|sectiontitle|redirectsnippet|redirecttitle|hasrelated'
 search_params = 'list=search&srwhat=text&srlimit=50&srsearch='
 new_page_params = 'list=recentchanges&rclimit=50&rctype=new&rcnamespace=0&rcshow=!redirect'
 backlink_params = 'list=backlinks&bllimit=500&blnamespace=0&bltitle='
 redirect_params = 'list=backlinks&blfilterredir=redirects&bllimit=500&blnamespace=0&bltitle='
 content_params = 'prop=revisions&rvprop=content|timestamp&titles='
+content_params2 = 'prop=revisions|info&rvprop=content|timestamp&titles='
 link_params = 'prop=links&pllimit=500&plnamespace=0&titles='
 templates_params = 'prop=templates&tllimit=500&tlnamespace=10&titles='
 allpages_params = 'list=allpages&apnamespace=0&apfilterredir=nonredirects&aplimit=500&apprefix='
@@ -73,7 +75,20 @@ def web_get(params):
         out.close()
     return json.loads(data)
 
+def web_post(params):
+    data = urllib.urlopen('https://en.wikipedia.org/w/api.php', 'format=json&action=query&' + params).read()
+    if save_to_cache:
+        out = open('cache/' + str(time()), 'w')
+        print >> out, params
+        print >> out, data
+        out.close()
+    return json.loads(data)
+
+re_end_parens = re.compile(r' \(.*\)$')
 def wiki_search(q):
+    m = re_end_parens.search(q)
+    if m:
+        q = q[:m.start()]
     search_url = search_params + urlquote('"%s"' % q)
     ret = web_get(search_url)
     totalhits = ret['query']['searchinfo']['totalhits']
@@ -165,7 +180,7 @@ def page_links(titles):
     return dict((doc['title'], set(l['title'] for l in doc['links'])) for doc in ret['query']['pages'].itervalues() if 'links' in doc)
 
 def is_disambig(doc):
-    return any('disambig' in t or t.endswith('dis') or t == 'template:surname' for t in (t['title'].lower() for t in doc.get('templates', [])))
+    return any('disambig' in t or t.endswith('dis') or 'given name' in t or t == 'template:surname' for t in (t['title'].lower() for t in doc.get('templates', [])))
 
 def test_is_disambig():
     assert not is_disambig({})
@@ -241,7 +256,17 @@ This sentence contains the test phrase.'''
     assert c == content.replace(tp, '[[' + tp + ']]')
     assert r == tp
 
-re_cite = re.compile('<ref>\s*{{cite.*}}\s*</ref>', re.I | re.S)
+def test_parse_cite():
+    sample = open('cite_parse_error').read().decode('utf-8')
+    found_duty = False
+    for a, b in parse_cite(sample):
+        if 'duty' in b.lower():
+            print (a, b)
+            found_duty = True
+    assert found_duty
+
+
+re_cite = re.compile('<ref>\s*{{cite.*?}}\s*</ref>', re.I | re.S)
 def parse_cite(text):
     prev = 0
     for m in re_cite.finditer(text):
@@ -286,6 +311,27 @@ def test_find_link_in_content():
     assert c == otrain.replace('turnstile', '[[turnstile]]')
     assert r == 'turnstile'
 
+    sample = """On April 26, 2006, Snoop Dogg and members of his entourage were arrested after being turned away from [[British Airways]]' first class lounge at [[Heathrow Airport]]. Snoop and his party were not allowed to enter the lounge because some of the entourage were flying first class, other members in economy class. After the group was escorted outside, they vandalized a duty-free shop by throwing whiskey bottles. Seven police officers were injured in the midst of the disturbance. After a night in prison, Snoop and the other men were released on bail on April 27, but he was unable to perform at the Premier Foods People's Concert in [[Johannesburg]] on the same day. As part of his bail conditions, he had to return to the police station in May. The group has been banned by British Airways for "the foreseeable future."<ref>{{cite news|url=http://news.bbc.co.uk/1/hi/entertainment/4949430.stm |title=Rapper Snoop Dogg freed on bail |publisher=BBC News  |date=April 27, 2006 |accessdate=January 9, 2011}}</ref><ref>{{cite news|url=http://news.bbc.co.uk/1/hi/entertainment/4953538.stm |title=Rap star to leave UK after arrest |publisher=BBC News  |date=April 28, 2006 |accessdate=January 9, 2011}}</ref> When Snoop Dogg appeared at a London police station on May 11, he was cautioned for [[affray]] under [[Fear or Provocation of Violence|Section 4]] of the [[Public Order Act 1986|Public Order Act]] for use of threatening words or behavior.<ref>{{cite news|url=http://newsvote.bbc.co.uk/1/hi/entertainment/4761553.stm|title=Rap star is cautioned over brawl |date=May 11, 2006|publisher=BBC News |accessdate=July 30, 2009}}</ref> On May 15, the [[Home Office]] decided that Snoop Dogg should be denied entry to the United Kingdom for the foreseeable future due to the incident at Heathrow as well as his previous convictions in the United States for drugs and firearms offenses.<ref>{{cite web|url=http://soundslam.com/articles/news/news.php?news=060516_snoopb |title=Soundslam News |publisher=Soundslam.com |date=May 16, 2006 |accessdate=January 9, 2011}}</ref><ref>{{cite web|url=http://uk.news.launch.yahoo.com/dyna/article.html?a=/060516/340/gbrj1.html&e=l_news_dm |title=Snoop 'banned from UK' |publisher=Uk.news.launch.yahoo.com |accessdate=January 9, 2011}}</ref> Snoop Dogg's visa card was rejected by local authorities on March 24, 2007 because of the Heathrow incident.<ref>{{cite news |first=VOA News |title=Rapper Snoop Dogg Arrested in UK |date=April 27, 2006 |publisher=Voice of America |url=http://classic-web.archive.org/web/20060603120934/http://voanews.com/english/archive/2006-04/2006-04-27-voa17.cfm |work=VOA News |accessdate=December 31, 2008}}</ref> A concert at London's Wembley Arena on March 27 went ahead with Diddy (with whom he toured Europe) and the rest of the show."""
+
+    (c, r) = find_link_in_content('duty-free shop', sample)
+    assert c == sample.replace('duty-free shop', '[[duty-free shop]]')
+    assert r == 'duty-free shop'
+
+    pseudocode1 = 'These languages are typically [[Dynamic typing|dynamically typed]], meaning that variable declarations and other [[Boilerplate_(text)#Boilerplate_code|boilerplate code]] can be omitted.'
+    (c, r) = find_link_in_content('boilerplate code', pseudocode1)
+    assert c == pseudocode1.replace('Boilerplate_(text)#Boilerplate_code|', '')
+    assert r == 'boilerplate code'
+
+    pseudocode2 = 'Large amounts of [[boilerplate (text)#Boilerplate code|boilerplate]] code, such as manual definitions of type casting macros and obscure type registration incantations, are necessary to create a new class.'
+    (c, r) = find_link_in_content('boilerplate code', pseudocode2)
+    assert c == pseudocode2.replace('(text)#Boilerplate code|boilerplate]] code', 'code]]')
+    assert r == 'boilerplate code'
+
+    yard = "Allentown is a regional center for commercial freight rail traffic. Currently, [[Norfolk Southern Railway|Norfolk Southern's]] primary [[Hump yard|hump classification yards]] are located in Allentown."
+    (c, r) = find_link_in_content('classification yard', yard)
+    assert c == yard.replace('[[Hump yard|hump classification yards]]', 'hump [[classification yard]]s')
+    assert r == 'classification yard'
+
     station = 'Ticket barriers control access to all platforms, although the bridge entrance has no barriers.'
     (c, r) = find_link_in_content('ticket barriers', station, linkto='turnstile')
     assert c == station.replace('Ticket barriers', '[[Turnstile|Ticket barriers]]')
@@ -303,6 +349,7 @@ def test_find_link_in_content():
         'Able to find this test\nPhrase in an article.', 
         'Able to find this [[test]] phrase in an article.',
         'Able to find this TEST [[PHRASE]] in an article.', 
+        'Able to find this [[testing|test]] phrase in an article.',
         'Able to find this testphrase in an article.']
 
     for input_content in content:
@@ -360,15 +407,17 @@ trans2 = { ' ': r"('?s?\]\])?'?s? ?(\[\[)?" }
 trans2[en_dash] = trans2[' ']
 
 patterns = [
+    lambda q: re.compile('\[\[[^|]+\|(%s)%s\]\]' % (q[0], q[1:]), re.I),
+    lambda q: re.compile('\[\[[^|]+\|(%s)%s(?:\]\])?' % (q[0], ''.join('-?' + trans2.get(c, c) for c in q[1:])), re.I),
     lambda q: re.compile('(%s)%s' % (q[0], q[1:]), re.I),
     lambda q: re.compile('(%s)%s' % (q[0], ''.join(trans.get(c, c) for c in q[1:])), re.I),
-    lambda q: re.compile(r'(?:\[\[)?(%s)%s(?:\]\])?' % (q[0], ''.join('-?' + trans2.get(c, c) for c in q[1:])), re.I),
+    lambda q: re.compile(r'(?:\[\[(?:[^]]+\|)?)?(%s)%s(?:\]\])?' % (q[0], ''.join('-?' + trans2.get(c, c) for c in q[1:])), re.I),
 ]
 
 def test_patterns():
     q = 'San Francisco'
-    assert patterns[0](q).pattern == '(S)' + q[1:]
-    assert patterns[1](q).pattern == '(S)an *[-\n]? *' + q[4:]
+    assert patterns[2](q).pattern == '(S)' + q[1:]
+    assert patterns[3](q).pattern == '(S)an *[-\n]? *' + q[4:]
 
 def match_found(m, q, linkto):
     if q[1:] == m.group(0)[1:]:
@@ -416,6 +465,36 @@ def find_link_in_content(q, content, linkto=None):
             return (new_content, replacement)
     raise NoMatch
 
+def find_link_and_section(q, content, linkto=None):
+    if linkto:
+        try:
+            return find_link_and_section(linkto, content)
+        except NoMatch:
+            pass
+    re_link = re.compile('([%s%s])%s' % (q[0].lower(), q[0].upper(), q[1:]))
+    sections = list(section_iter(content))
+    replacement = None
+    for pattern in patterns:
+        re_link = pattern(q)
+        for section_num, (header, section_text) in enumerate(sections):
+            new_content = ''
+            if header:
+                new_content += header 
+            for token_type, text in parse_cite(section_text):
+                if token_type == 'text' and not replacement:
+                    m = re_link.search(text)
+                    if m:
+                        replacement = match_found(m, q, linkto)
+                        text = re_link.sub(lambda m: "[[%s]]" % replacement, text, count=1)
+                new_content += text
+            if replacement:
+                return {
+                    'section_num': section_num,
+                    'section_text': new_content.strip(),
+                    'replacement': replacement,
+                }
+    raise NoMatch
+
 def test_get_case_from_content(): # test is broken
     global web_get
     title = 'London congestion charge'
@@ -434,10 +513,32 @@ def get_case_from_content(title):
     if start != -1:
         return content[start+3:start+3+len(title)]
 
-@app.route('/diff/<q>')
-def diff_view(q):
+@app.route('/diff')
+def diff_view():
+    q = request.args.get('q')
     title = request.args.get('title')
-    return render_template('diff.html', q=q, title=title)
+    linkto = request.args.get('linkto')
+
+    ret = web_get(content_params2 + urlquote(title))
+    page = ret['query']['pages'].values()[0]
+    #edittoken = page['edittoken']
+    #print 'token:', edittoken
+    rev = page['revisions'][0]
+    content = rev['*']
+    try:
+        found = find_link_and_section(q, content, linkto)
+    except NoMatch:
+        return None
+
+    #print found['section_num'], found['replacement'], len(found['section_text'])
+
+    diff_params = "prop=revisions&rvprop=timestamp&titles=%s&rvsection=%d&rvdifftotext=%s" % (urlquote(title), found['section_num'], urlquote(found['section_text']))
+
+    ret = web_post(diff_params)
+    diff = ret['query']['pages'].values()[0]['revisions'][0]['diff']['*']
+    #pprint(ret['query']['pages'].values()[0])
+
+    return '<table>' + diff + '</table>'
 
 def get_page(title, q, linkto=None):
     ret = web_get(content_params + urlquote(title))
@@ -450,6 +551,9 @@ def get_page(title, q, linkto=None):
         (content, replacement) = find_link_in_content(q, content, linkto)
     except NoMatch:
         return None
+
+    diff_url = "prop=revisions&rvprop=timestamp&titles=%s&rvdifftotext=%s" % (urlquote(title), urlquote(content))
+    #pprint(web_post(diff_url), stream=open('wikidiff', 'w'))
 
     summary = "link [[%s]] using [[User:Edward/Find link|Find link]]" % replacement
     #text = "title: %s\nq: %s\nsummary: %s\ntimestamp: %s\n\n%s" % (title, q, timestamp, summary, content)
@@ -498,7 +602,6 @@ def test_match_type():
 
 @app.route("/<q>")
 def findlink(q, title=None, message=None):
-
     q_trim = q.strip('_')
     if not message and (' ' in q or q != q_trim):
         return redirect(url_for('findlink', q=q.replace(' ', '_').strip('_'), message=message))
@@ -559,6 +662,7 @@ def findlink(q, title=None, message=None):
         results = search,
         urlquote = urlquote,
         commify = commify,
+        enumerate = enumerate,
         longer_titles = longer,
         redirect_to = redirect_to,
         norm_match_redirect = norm_match_redirect,
@@ -591,15 +695,14 @@ def index():
         if linkto:
             linkto = wiki_space_norm(linkto)
         reply = get_page(title, q, linkto)
-        if reply is None:
-            redirects = list(wiki_redirects(q))
-            for r in redirects:
-                reply = get_page(title, r, linkto=q)
-                if reply:
-                    return reply
-            return findlink(q.replace(' ', '_'), title=title, message=q + ' not in ' + title)
-        else:
+        if reply:
             return reply
+        redirects = list(wiki_redirects(q))
+        for r in redirects:
+            reply = get_page(title, r, linkto=q)
+            if reply:
+                return reply
+        return findlink(q.replace(' ', '_'), title=title, message=q + ' not in ' + title)
     if q:
         return redirect(url_for('findlink', q=q.replace(' ', '_').strip('_')))
     return render_template('index.html')
