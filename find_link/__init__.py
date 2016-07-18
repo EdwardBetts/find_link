7de778adc02ba4392c04eaf4ac0bf30c40cc04d5
Edward Betts <edwardbetts@gmail.com>
2016-07-18 11:26:15 +0100

    better error handling. fix unquote bug

--- find_link/__init__.py
+++ find_link/__init__.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
 from flask import Flask
 import find_link.view
+from .error_mail import setup_error_mail
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config')
     find_link.view.init_app(app)
+    setup_error_mail(app)
     return app
