9047475a136d5eb8a637e95830d51b47f64007be
Edward Betts <edward@4angle.com>
2016-07-30 07:26:37 +0100

    include exception in error subject line

--- find_link/error_mail.py
+++ find_link/error_mail.py
@@ -2,11 +2,18 @@ import logging
 from logging.handlers import SMTPHandler
 from logging import Formatter
 
+class MySMTPHandler(SMTPHandler):
+    def getSubject(self, record):
+        if record.exc_info:
+            return 'find_link error: {}'.format(record.exc_info[0])
+        return 'find_link error: {}:{:d}'.format(record.pathname,
+                                                 record.lineno)
+
 def setup_error_mail(app):
-    mail_handler = SMTPHandler(app.config['SMTP_HOST'],
-                               app.config['MAIL_FROM'],
-                               app.config['ADMINS'],
-                               app.name + ' error')
+    mail_handler = MySMTPHandler(app.config['SMTP_HOST'],
+                                 app.config['MAIL_FROM'],
+                                 app.config['ADMINS'],
+                                 app.name + ' error')
     mail_handler.setFormatter(Formatter('''
     Message type:       %(levelname)s
     Location:           %(pathname)s:%(lineno)d
