97c796ca0d50330616c4525ea8bc6f138dd0fa7f
Edward Betts <edward@4angle.com>
2023-01-09 13:59:03 +0000

    Format code with black, add docstrings and type hints

--- find_link/error_mail.py
+++ find_link/error_mail.py
@@ -1,20 +1,32 @@
+"""Send mail to admin when an error happens."""
+
 import logging
-from logging.handlers import SMTPHandler
-from logging import Formatter
+import logging.handlers
 
-class MySMTPHandler(SMTPHandler):
-    def getSubject(self, record):
+import flask
+
+
+class MySMTPHandler(logging.handlers.SMTPHandler):
+    """Custom SMTP handler to change mail subject."""
+
+    def getSubject(self, record: logging.LogRecord) -> str:
+        """Specify subject line for error mails."""
         if record.exc_info and record.exc_info[0]:
-            return 'find_link error: {}'.format(record.exc_info[0].__name__)
-        return 'find_link error: {}:{:d}'.format(record.pathname,
-                                                 record.lineno)
+            return "find_link error: {}".format(record.exc_info[0].__name__)
+        return "find_link error: {}:{:d}".format(record.pathname, record.lineno)
 
-def setup_error_mail(app):
-    mail_handler = MySMTPHandler(app.config['SMTP_HOST'],
-                                 app.config['MAIL_FROM'],
-                                 app.config['ADMINS'],
-                                 app.name + ' error')
-    mail_handler.setFormatter(Formatter('''
+
+def setup_error_mail(app: flask.Flask) -> None:
+    """Send mail to admins when an error happens."""
+    mail_handler = MySMTPHandler(
+        app.config["SMTP_HOST"],
+        app.config["MAIL_FROM"],
+        app.config["ADMINS"],
+        app.name + " error",
+    )
+    mail_handler.setFormatter(
+        logging.Formatter(
+            """
     Message type:       %(levelname)s
     Location:           %(pathname)s:%(lineno)d
     Module:             %(module)s
@@ -24,8 +36,9 @@ def setup_error_mail(app):
     Message:
 
     %(message)s
-    '''))
+    """
+        )
+    )
 
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
-
