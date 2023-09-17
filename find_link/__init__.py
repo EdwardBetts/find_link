a849d30e50a132198d8bc2470cf699ba5c1f9c41
Edward Betts <edward@4angle.com>
2023-09-17 07:44:46 +0530

    Format code with black, add types and docstrings.

--- find_link/__init__.py
+++ find_link/__init__.py
@@ -1,10 +1,26 @@
+import builtins
+import types
+from typing import Type
+
 from flask import Flask, request
+
 import find_link.view
+
 from .error_mail import setup_error_mail
 
+ExcInfo = (
+    tuple[Type[builtins.BaseException], builtins.BaseException, types.TracebackType]
+    | tuple[None, None, None]
+)
+
+
 class MyFlask(Flask):
-    def log_exception(self, exc_info):
-        self.logger.error("""
+    """Subclass of Flask for better log mails."""
+
+    def log_exception(self, exc_info: ExcInfo) -> None:
+        """Log exception with more detail."""
+        self.logger.error(
+            """
 Path:                 %s
 HTTP Method:          %s
 Client IP Address:    %s
@@ -15,22 +31,27 @@ User Browser Version: %s
 GET args:             %s
 view args:            %s
 URL:                  %s
-""" % (
-            request.path,
-            request.method,
-            request.remote_addr,
-            request.user_agent.string,
-            request.user_agent.platform,
-            request.user_agent.browser,
-            request.user_agent.version,
-            dict(request.args),
-            request.view_args,
-            request.url
-        ), exc_info=exc_info)
+"""
+            % (
+                request.path,
+                request.method,
+                request.remote_addr,
+                request.user_agent.string,
+                request.user_agent.platform,
+                request.user_agent.browser,
+                request.user_agent.version,
+                dict(request.args),
+                request.view_args,
+                request.url,
+            ),
+            exc_info=exc_info,
+        )
 
-def create_app():
+
+def create_app() -> MyFlask:
+    """Create application."""
     app = MyFlask(__name__)
-    app.config.from_pyfile('config')
+    app.config.from_pyfile("config")
     find_link.view.init_app(app)
     setup_error_mail(app)
     return app
