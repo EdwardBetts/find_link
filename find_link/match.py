55f77bc899eced42268bbc94a5ac177dad6033a9
Edward Betts <edward@4angle.com>
2023-09-17 07:38:53 +0530

    Format code with black.

--- find_link/match.py
+++ find_link/match.py
@@ -1,55 +1,88 @@
 from __future__ import unicode_literals
-from .util import is_title_case, lc_alpha
-from .core import get_content_and_timestamp, get_case_from_content
-from .api import get_wiki_info, call_get_diff, MissingPage
+
+import collections
 import re
 
-re_link_in_text = re.compile(r'\[\[[^]]+?\]\]', re.I | re.S)
+from .api import MissingPage, call_get_diff, get_wiki_info
+from .core import get_case_from_content, get_content_and_timestamp
+from .util import is_title_case, lc_alpha
+
+re_link_in_text = re.compile(r"\[\[[^]]+?\]\]", re.I | re.S)
+
 
 class LinkReplace(Exception):
-    pass
+    """Link is being replaced."""
 
 
-en_dash = u'\u2013'
-trans = {',': ',?', ' ': ' *[-\n]? *'}
-trans[en_dash] = trans[' ']
+en_dash = "\u2013"
+trans = {",": ",?", " ": " *[-\n]? *"}
+trans[en_dash] = trans[" "]
 
-trans2 = {' ': r"('?s?\]\])?'?s? ?(\[\[(?:.+\|)?)?", '-': '[- ]'}
-trans2[en_dash] = trans2[' ']
+trans2 = {" ": r"('?s?\]\])?'?s? ?(\[\[(?:.+\|)?)?", "-": "[- ]"}
+trans2[en_dash] = trans2[" "]
 
 patterns = [
-    lambda q: re.compile(r'(?<!-)(?:\[\[(?:[^]]+\|)?)?(%s)%s(?:\]\])?' % (re.escape(q[0]), ''.join('-?' + (trans2[c] if c in trans2 else re.escape(c)) for c in q[1:])), re.I),
-    lambda q: re.compile(r'(?<!-)\[\[[^|]+\|(%s)%s\]\]' % (re.escape(q[0]), re.escape(q[1:])), re.I),
-    lambda q: re.compile(r'(?<!-)\[\[[^|]+\|(%s)%s(?:\]\])?' % (re.escape(q[0]), ''.join('-?' + (trans2[c] if c in trans2 else re.escape(c)) for c in q[1:])), re.I),
-    lambda q: re.compile(r'(?<!-)(%s)%s' % (re.escape(q[0]), re.escape(q[1:])), re.I),
-    lambda q: re.compile(r'(?<!-)(%s)%s' % (re.escape(q[0]), ''.join((trans[c] if c in trans else re.escape(c)) for c in q[1:])), re.I),
+    lambda q: re.compile(
+        r"(?<!-)(?:\[\[(?:[^]]+\|)?)?(%s)%s(?:\]\])?"
+        % (
+            re.escape(q[0]),
+            "".join("-?" + (trans2[c] if c in trans2 else re.escape(c)) for c in q[1:]),
+        ),
+        re.I,
+    ),
+    lambda q: re.compile(
+        r"(?<!-)\[\[[^|]+\|(%s)%s\]\]" % (re.escape(q[0]), re.escape(q[1:])), re.I
+    ),
+    lambda q: re.compile(
+        r"(?<!-)\[\[[^|]+\|(%s)%s(?:\]\])?"
+        % (
+            re.escape(q[0]),
+            "".join("-?" + (trans2[c] if c in trans2 else re.escape(c)) for c in q[1:]),
+        ),
+        re.I,
+    ),
+    lambda q: re.compile(r"(?<!-)(%s)%s" % (re.escape(q[0]), re.escape(q[1:])), re.I),
+    lambda q: re.compile(
+        r"(?<!-)(%s)%s"
+        % (
+            re.escape(q[0]),
+            "".join((trans[c] if c in trans else re.escape(c)) for c in q[1:]),
+        ),
+        re.I,
+    ),
 ]
 
+
 class NoMatch(Exception):
-    pass
+    """No match found."""
 
 
-re_cite = re.compile(r'<ref( [^>]*?)?>\s*({{cite.*?}}|\[https?://[^]]*?\])\s*</ref>', re.I | re.S)
+re_cite = re.compile(
+    r"<ref( [^>]*?)?>\s*({{cite.*?}}|\[https?://[^]]*?\])\s*</ref>", re.I | re.S
+)
 
-def parse_cite(text):
+
+def parse_cite(text: str) -> collections.abc.Iterator[tuple[str, str]]:
+    """Parse a citation."""
     prev = 0
     for m in re_cite.finditer(text):
-        yield ('text', text[prev:m.start()])
-        yield ('cite', m.group(0))
+        yield ("text", text[prev : m.start()])
+        yield ("cite", m.group(0))
         prev = m.end()
-    yield ('text', text[prev:])
+    yield ("text", text[prev:])
 
 
-re_heading = re.compile(r'^\s*(=+)\s*(.+)\s*\1(<!--.*-->|\s)*$')
+re_heading = re.compile(r"^\s*(=+)\s*(.+)\s*\1(<!--.*-->|\s)*$")
+
 
 def section_iter(text):
-    cur_section = ''
+    cur_section = ""
     heading = None
     in_comment = False
     for line in text.splitlines(True):
-        if '<!--' in line:
+        if "<!--" in line:
             in_comment = True
-        if '-->' in line:
+        if "-->" in line:
             in_comment = False
         m = re_heading.match(line)
         if in_comment or not m:
@@ -58,13 +91,14 @@ def section_iter(text):
         if cur_section or heading:
             yield (heading, cur_section)
         heading = m.group()
-        cur_section = ''
+        cur_section = ""
         continue
     yield (heading, cur_section)
 
+
 def get_subsections(text, section_num):
-    'retrieve the text of subsections for a given section number within an article'
-    found = ''
+    "retrieve the text of subsections for a given section number within an article"
+    found = ""
     collection_level = None
     for num, (heading, body) in enumerate(section_iter(text)):
         if heading is None:
@@ -81,6 +115,7 @@ def get_subsections(text, section_num):
             else:
                 break
     return found
+
 
 def match_found(m, q, linkto):
     if q[1:] == m.group(0)[1:]:
@@ -100,21 +135,26 @@ def match_found(m, q, linkto):
             linkto = linkto[0].lower() + linkto[1:]
         elif replacement[0].isupper():
             linkto = linkto[0].upper() + linkto[1:]
-        replacement = linkto + '|' + replacement
+        replacement = linkto + "|" + replacement
     return replacement
+
 
 def parse_links(text):
     prev = 0
     for m in re_link_in_text.finditer(text):
         if prev != m.start():
-            yield ('text', text[prev:m.start()])
-        if any(m.group().lower().startswith('[[' + prefix) for prefix in ('file:', 'image:')):
-            yield ('image', m.group(0))
+            yield ("text", text[prev : m.start()])
+        if any(
+            m.group().lower().startswith("[[" + prefix)
+            for prefix in ("file:", "image:")
+        ):
+            yield ("image", m.group(0))
         else:
-            yield ('link', m.group(0))
+            yield ("link", m.group(0))
         prev = m.end()
     if prev < len(text):
-        yield ('text', text[prev:])
+        yield ("text", text[prev:])
+
 
 def mk_link_matcher(q):
     re_links = [p(q) for p in patterns]
@@ -122,17 +162,19 @@ def mk_link_matcher(q):
     def search_for_link(text):
         for re_link in re_links:
             m = re_link.search(text)
-            if m and m.group(0).count('[[') < 4:
+            if m and m.group(0).count("[[") < 4:
                 return m
 
     return search_for_link
 
+
 def add_link(m, replacement, text):
     return m.re.sub(lambda m: "[[%s]]" % replacement, text, count=1)
 
+
 def find_link_in_chunk(q, content, linkto=None):
     search_for_link = mk_link_matcher(q)
-    new_content = ''
+    new_content = ""
     replacement = None
 
     match_in_non_link = False
@@ -140,26 +182,30 @@ def find_link_in_chunk(q, content, linkto=None):
     found_text_to_link = None
 
     for token_type, text in parse_links(content):
-        if token_type == 'text':
+        if token_type == "text":
             if search_for_link(text):
                 match_in_non_link = True
-        elif token_type == 'image':
-            before, sep, link_text = text[:-2].rpartition('|')
+        elif token_type == "image":
+            before, sep, link_text = text[:-2].rpartition("|")
             m = search_for_link(link_text)
             if m:
                 found_text_to_link = m.group(0)
                 replacement = match_found(m, q, linkto)
-                text = before + sep + add_link(m, replacement, link_text) + ']]'
-        elif token_type == 'link' and not replacement and not match_in_non_link:
+                text = before + sep + add_link(m, replacement, link_text) + "]]"
+        elif token_type == "link" and not replacement and not match_in_non_link:
             link_text = text[2:-2]
             link_dest = None
-            if '|' in link_text:
-                link_dest, link_text = link_text.split('|', 1)
+            if "|" in link_text:
+                link_dest, link_text = link_text.split("|", 1)
             m = search_for_link(link_text)
-            if m and (not link_dest or not link_dest.startswith('#')):
+            if m and (not link_dest or not link_dest.startswith("#")):
                 lc_alpha_q = lc_alpha(q)
 
-                bad_link_match = link_dest and len(link_dest) > len(q) and (lc_alpha_q not in lc_alpha(link_dest))
+                bad_link_match = (
+                    link_dest
+                    and len(link_dest) > len(q)
+                    and (lc_alpha_q not in lc_alpha(link_dest))
+                )
                 if not link_dest:
                     if q in link_text and len(link_text) > len(q):
                         bad_link_match = True
@@ -168,7 +214,10 @@ def find_link_in_chunk(q, content, linkto=None):
                         link_dest_redirect = get_wiki_info(link_dest)
                     except MissingPage:
                         link_dest_redirect = None
-                    if link_dest_redirect and lc_alpha(link_dest_redirect) == lc_alpha_q:
+                    if (
+                        link_dest_redirect
+                        and lc_alpha(link_dest_redirect) == lc_alpha_q
+                    ):
                         bad_link_match = False
                 if not bad_link_match:
                     replacement = match_found(m, q, linkto)
@@ -185,18 +234,20 @@ def find_link_in_chunk(q, content, linkto=None):
             new_content = add_link(m, replacement, content)
             if linkto:
                 m_end = m.end()
-                re_extend = re.compile(m.re.pattern + r'\w*\b', re.I)
+                re_extend = re.compile(m.re.pattern + r"\w*\b", re.I)
                 m = re_extend.search(content)
                 if m and m.end() > m_end:
-                    replacement += content[m_end:m.end()]
+                    replacement += content[m_end : m.end()]
                     new_content = add_link(m, replacement, content)
     return (new_content, replacement, found_text_to_link)
+
 
 def find_link_in_text(q, content):
     (new_content, replacement) = find_link_in_chunk(q, content)
     if replacement:
         return (new_content, replacement)
     raise NoMatch
+
 
 def find_link_in_content(q, content, linkto=None):
     if linkto:
@@ -205,15 +256,17 @@ def find_link_in_content(q, content, linkto=None):
         except NoMatch:
             pass
     replacement = None
-    new_content = ''
+    new_content = ""
     link_replace = False
     for header, section_text in section_iter(content):
         if header:
             new_content += header
         for token_type, text in parse_cite(section_text):
-            if token_type == 'text' and not replacement:
+            if token_type == "text" and not replacement:
                 try:
-                    (new_text, replacement, replaced_text) = find_link_in_chunk(q, text, linkto=linkto)
+                    (new_text, replacement, replaced_text) = find_link_in_chunk(
+                        q, text, linkto=linkto
+                    )
                 except LinkReplace:
                     link_replace = True
                 if replacement:
@@ -222,6 +275,7 @@ def find_link_in_content(q, content, linkto=None):
     if replacement:
         return (new_content, replacement, replaced_text)
     raise LinkReplace if link_replace else NoMatch
+
 
 def find_link_and_section(q, content, linkto=None):
     if linkto:
@@ -237,24 +291,24 @@ def find_link_and_section(q, content, linkto=None):
     found = {}
 
     for section_num, (header, section_text) in enumerate(sections):
-        new_content = ''
+        new_content = ""
         if header:
             new_content += header
         for token_type, text in parse_cite(section_text):
-            if token_type == 'text' and not replacement:
-                new_text = ''
+            if token_type == "text" and not replacement:
+                new_text = ""
                 for token_type2, text2 in parse_links(text):
-                    if token_type2 == 'link' and not replacement:
+                    if token_type2 == "link" and not replacement:
                         link_text = text2[2:-2]
-                        if '|' in link_text:
-                            link_dest, link_text = link_text.split('|', 1)
+                        if "|" in link_text:
+                            link_dest, link_text = link_text.split("|", 1)
                         else:
                             link_dest = None
                         m = search_for_link(link_text)
                         if m:
                             if link_dest:
-                                found['link_dest'] = link_dest
-                            found['link_text'] = link_text
+                                found["link_dest"] = link_dest
+                            found["link_text"] = link_text
                             replacement = match_found(m, q, None)
                             text2 = add_link(m, replacement, link_text)
                     new_text += text2
@@ -267,20 +321,25 @@ def find_link_and_section(q, content, linkto=None):
                         text = add_link(m, replacement, text)
             new_content += text
         if replacement:
-            found.update({
-                'section_num': section_num,
-                'section_text': new_content,
-                'old_text': (header or '') + section_text,
-                'replacement': replacement,
-            })
+            found.update(
+                {
+                    "section_num": section_num,
+                    "section_text": new_content,
+                    "old_text": (header or "") + section_text,
+                    "replacement": replacement,
+                }
+            )
             return found
     raise NoMatch
 
-def get_diff(q, title, linkto):
+
+def get_diff(q: str, title: str, linkto: str) -> tuple[str, str]:
     content, timestamp = get_content_and_timestamp(title)
     found = find_link_and_section(q, content, linkto)
 
-    section_text = found['section_text'] + get_subsections(content, found['section_num'])
+    section_text = found["section_text"] + get_subsections(
+        content, found["section_num"]
+    )
 
-    diff = call_get_diff(title, found['section_num'], section_text)
-    return (diff, found['replacement'])
+    diff = call_get_diff(title, found["section_num"], section_text)
+    return (diff, found["replacement"])
